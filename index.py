dfa9b28f87d4c0afc9abcd1c90e7bbb56d682cc3
hamud-commits <pkkmialkhairiyahbds@gmail.com>
2026-06-22 23:47:30 +0700

    fix: update vercel config

--- index.py
+++
@@ -1,7 +0,0 @@
-# api/index.py
-
-# Mengimpor 'app' dari file app.py kamu
-from app import app
-
-# Vercel menggunakan variabel 'app' ini sebagai entry point 
-# untuk menjalankan aplikasi Flask kamu secara serverless.
